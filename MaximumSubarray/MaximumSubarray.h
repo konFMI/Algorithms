48587b4bfd1a27051bbe0dec9cbee05591655ba4
=konFMI <33687265+konFMI@users.noreply.github.com>
2020-05-16 11:37:11 -0700

    Used the algorithm from MIT book Introduction to Algorithms.

---
+++ MaximumSubarray/MaximumSubarray.h
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<tuple>
+#include<limits>
+
+using std::tuple;
+using std::make_tuple;
+using std::get;
+using std::numeric_limits;
+
+tuple<int,int,int> FindMaxCrossingSubarray(int* array, int low, int mid, int high)
+{
+    tuple<int,int,int> data;
+    int leftSum = numeric_limits<int>::min();
+    int maxLeftIndex = mid;
+    int sum = 0;
+    for (int i = mid; i >= low; i--)
+    {
+
+        sum += array[i];
+        if ( sum > leftSum)
+        {
+            leftSum = sum;
+            maxLeftIndex = i;
+        }
+        
+    }
+
+
+    int rightSum = numeric_limits<int>::min();
+    int maxRightIndex = mid;
+    sum = 0;
+
+    for (size_t i = mid + 1; i <= high; i++)
+    {
+        sum += array[i];
+        if(sum > rightSum)
+        {
+            rightSum = sum;
+            maxRightIndex = i;
+        }
+    }
+    
+    data = make_tuple(maxLeftIndex,maxRightIndex,leftSum+rightSum);
+    return data;
+}
+
+tuple<int,int,int> FindMaxSubarray(int* array, int low, int high)
+{
+
+    if (low == high)
+    {
+        return make_tuple(low,high,array[low]); // There is one element;
+    }
+
+    int mid = (low+high) / 2;
+
+    tuple<int,int,int> leftSubarray = FindMaxSubarray(array,low,mid);
+    tuple<int,int,int> righSubarray = FindMaxSubarray(array, mid + 1, high);
+    tuple<int,int,int> crossingSubarray = FindMaxCrossingSubarray(array,low,mid,high);
+
+    int leftSum = get<2>(leftSubarray);
+    int rightSum = get<2>(righSubarray);
+    int crossSum = get<2>(crossingSubarray);
+    if (leftSum >= rightSum && leftSum >= crossSum)
+    {
+        return leftSubarray;
+    }
+    else if (rightSum >= leftSum && rightSum >= crossSum)
+    {
+        return righSubarray;
+    }
+    else
+    {
+        return crossingSubarray;
+    }
+}
---
+++ MaximumSubarray/main.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include"MaximumSubarray.h"
+void TestCode()
+{
+    tuple<int,int,int> data;
+    int size = 0;
+    do
+    {
+        std::cout<<"Enter size of the array: ";
+        std::cin>>size;
+    } while (size < 0 && size > 10000);
+    
+    int* array = new int[size];
+    std::cout<<"Enter "<<size<<" values for the array."<<std::endl;
+    for (size_t i = 0; i < size; i++)
+    {
+        int inputValue = 0;
+        
+        while (!std::cin>>inputValue)
+        {
+            std::cout<<"Enter a valid integer."<<std::endl;
+        }
+        array[i] = inputValue;
+        
+    }
+    //1 2 3 -123 2 3 -23 3 4 1
+    int low = 0, high = size - 1;
+    int mid = (low+high) / 2;
+
+    data = FindMaxSubarray(array, low, high);
+    std::cout<<"The maximum subarray is:"<<std::endl;
+    std::cout<<"[ ";
+    for (size_t i = get<0>(data); i <= get<1>(data); i++)
+    {
+        std::cout<<array[i]<<" ";
+    }
+    std::cout<<"]"<<std::endl;
+    std::cout<<"With the maximum sum of "<<get<2>(data)<<std::endl;
+    delete array;
+}
+void TestCodeBook()
+{
+    int array[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+    tuple<int,int,int> data = FindMaxSubarray(array,0,15);
+    std::cout<<get<2>(data)<<std::endl;
+    for (size_t i = get<0>(data); i <= get<1>(data); i++)
+    {
+        std::cout<<array[i]<< " ";
+    }
+    std::cout<<"\nMax sum: "<<get<2>(data)<<"\n";
+    
+}
+int main(){
+
+    TestCode();
+
+    return 0;
+}
