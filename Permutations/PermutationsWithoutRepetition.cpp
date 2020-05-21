4a6dc5332bef37d3f2a7d25d021afa6d553daa46
=konFMI <33687265+konFMI@users.noreply.github.com>
2020-05-20 17:36:00 -0700

    Update PermutationsWithoutRepetition.cpp

--- Permutations/PermutationsWithoutRepetition.cpp
+++ Permutations/PermutationsWithoutRepetition.cpp
@@ -9,7 +9,7 @@ struct Solution
     {
         if(index >= elements.size())
         {
-            Print();
+            vectors.push_back(permutation);
         }
 
         for (size_t i = 0; i < elements.size(); i++)
@@ -25,27 +25,62 @@ struct Solution
         }
     
     }
-    void Print()
+    void WithRepetition(int index)
     {
-        for (size_t i = 0; i < permutation.size(); i++)
-    {
-        std::cout<<permutation[i]<<" ";
-    }
-        std::cout<<std::endl;
+        if(index >= elements.size())
+        {
+            vectors.push_back(permutation);
+        }
 
-        return;
+        for (size_t i = 0; i < elements.size(); i++)
+        {
+            
+            permutation[index] = elements[i];
+            WithRepetition(index + 1);
+            
+            
+        }
+    
     }
-
+    
     void AddElement(char element)
     {
         elements.push_back(element);
         permutation.push_back(' ');
         usedIndex.push_back(false);
     }
-
-    private: vector<char> elements;
-    private: vector<char> permutation;
-    private: vector<bool> usedIndex;
+    void Print()
+    {
+        for (size_t j = 0; j < vectors.size(); j++)
+        {
+            permutation = vectors[j];
+            RemoveDuplicate(permutation,j+1);
+            for (size_t i = 0; i < permutation.size(); i++)
+            {
+                std::cout<<permutation[i]<<" ";
+            }
+            std::cout<<std::endl;
+        }
+        return;
+    }
+    private:
+     void RemoveDuplicate(vector<char> vec, int startIndex)
+     {
+         for (int i = startIndex; i < vectors.size(); i++)
+         {
+             if (vectors[i] == vec)
+             {
+                 vectors.erase(vectors.begin() + i);
+             }
+             
+         }
+         
+     }
+    
+     vector<char> elements;
+     vector<char> permutation;
+     vector<bool> usedIndex;
+     vector<vector<char>> vectors;
 
 }permute;
 
@@ -56,12 +91,12 @@ int main()
 {
     permute.AddElement('A');
     permute.AddElement('B');
-    permute.AddElement('C');
+    permute.AddElement('B');
     permute.AddElement('D');
-    permute.AddElement('E');
+    permute.AddElement('F');
     permute.AddElement('F');
     
     permute.WithoutRepetition(0);
-
+    permute.Print();
     return 0;
 }
